add41a3653a43664806537a24f24fada4a2df241
Mohsen Vafa <mohsen.vafa@gmail.com>
2025-09-21 21:12:28 -0700

    Refactor patient list page; integrate DataTable component for improved table rendering and structure

---
+++ internal/ui/patient/patient-list/patient-table.go
@@ -0,0 +1,10 @@
+package patient_list
+
+import commonComponents "github.com/pharmacy-modernization-project-model/internal/ui/common/components"
+
+var patientTableColumns = []commonComponents.DataTableColumn{
+	{Title: "", Class: "w-0"},
+	{Title: "Patient"},
+	{Title: "Birthdate"},
+	{Title: "Prescriptions"},
+}
