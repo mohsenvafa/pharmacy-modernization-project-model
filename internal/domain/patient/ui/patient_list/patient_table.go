ce34c0ca2d1b012c2f06b99320af3c6624423d23
Mohsen Vafa <mohsen.vafa@gmail.com>
2025-09-22 21:54:55 -0700

    cp

--- internal/domain/patient/ui/patient_list/patient_table.go
+++ internal/domain/patient/ui/patient_list/patient_table.go
@@ -1,6 +1,6 @@
 package patient_list
 
-import commonComponents "github.com/pharmacy-modernization-project-model/internal/ui/common/components"
+import commonComponents "github.com/pharmacy-modernization-project-model/web/components/elements"
 
 var patientTableColumns = []commonComponents.DataTableColumn{
 	{Title: "", Class: "w-0"},
